29cad3607e2455146831e281a1fd88b77fe42a4d
Sunlit <sunil.tawde@gmail.com>
2012-04-12 11:05:41 -0700

    Added thank you message to default recipe

---
+++ cookbooks/sunlit/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Sunlit Source"
+maintainer_email "fake@email.com"
+license          "All rights reserved"
+description      "Installs/Configures sunlit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "sunlit::default", "Prints the thank you message"
